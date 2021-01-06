605fa508282f4ef1fff20f1a867991ba7cd434aa
GuilianavaD <guilianavaler@gmail.com>
2021-01-06 18:55:01 -0500

    Create LOGIN.php

---
+++ LOGIN.php
@@ -0,0 +1,38 @@
+<?php
+ $conexion = new mysqli('localhost', 'root','', 'diario',3310);
+ if ($conexion->connect_error) die ("Fatal error");
+ECHO "<center><h1>ALCOHÓLICOS ANÓNIMOS</h1>";
+echo <<<_END
+<html>
+    <head>
+        <title>AA</title>
+    </head>
+    <body style=background-image:url('imagen/fondoinvi.jpg width=100% height=100vh ')>
+    <div class="box_form">
+    <form method="post" action="Inicio.php">
+    <tr>¿Cual es tu Usuario?<p>
+        <input type="text" name="name" placeholder="Usuario"><p>
+        <input type="password" placeholder="CONTRASEÑA"><p>
+        <input type="submit"value="Ingresar"><p>
+        <img src="imagen/derota.jpg" width="350px" height="250" "aling="right">
+    </form>
+    <div>
+    
+    </body>
+</html>
+_END;
+function mysql_fix_string($coneccion, $string)
+{
+    if (get_magic_quotes_gpc())
+        $string = stripcslashes($string);
+    return $coneccion->real_escape_string($string);
+}
+function sanitizeString($var)
+{
+    if (get_magic_quotes_gpc())
+        $var = stripslashes($var);
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    return $var;
+}
+?>
