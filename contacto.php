7dcb0ac126b1c06a67ca9d880b122ddfa9addd49
GuilianavaD <guilianavaler@gmail.com>
2021-01-07 20:52:30 -0500

    Create contacto.php

---
+++ contacto.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA</title>
+    <style type="text/css">
+        
+        * {
+            margin:0px;
+            padding:0px;
+        }
+        
+        #header {
+            margin:auto;
+            width:500px;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+        
+        ul, ol {
+            list-style:none;
+        }
+        
+        .nav > li {
+            float:left;
+        }
+        
+        .nav li a {
+            background-color:rgb(30, 111, 204);
+            color:#fff;
+            text-decoration:none;
+            padding:10px 12px;
+            display:block;
+        }
+        
+        .nav li a:hover {
+            background-color:#0e2f9b;
+        }
+        
+        .nav li ul {
+            display:none;
+            position:absolute;
+            min-width:140px;
+        }
+        
+        .nav li:hover > ul {
+            display:block;
+        }
+        
+        .nav li ul li {
+            position:relative;
+        }
+        
+        .nav li ul li ul {
+            right:-140px;
+            top:0px;
+        }
+        div{
+          
+            background-color: lightblue;
+            margin: 10px;
+        }
+
+    </style>
+    </head>
+    <body style=background-image:url('imagen/fondoinvi.jpg width=100% height=100vh ')>
+    <div id="header">
+        <ul class="nav">
+            <li><a href="Inicio.php">INICIO</a>
+                <ul>
+                    <li><a href="Informacion.php">INFORMACIÓN</a>
+                
+                    </li>
+                    <li><a href="contacto.php">CONTACTOS</a>
+                    
+                    </li>
+                    <li><a href="misivisi.php">MISIÓN Y VISIÓN</a>
+                    </li>
+                    <li><a href="index.html">Cerrar Sesión</a>
+                </ul>
+            </li>
+            <li><a href="conceptos.php">CONCEPTOS</a>
+                <ul>
+                <li><a href="alcoholismo.php">EL ALCOHOLISMO</a>
+                </li>
+                </ul>
+            </li>
+
+            <li><a href="consejos.php">CONSEJOS</a>
+                <ul> 
+                    <li><a href="recomendacion.php">RECOMENDACIONES</a>
+                </ul>
+            </li>
+            <li><a href="autores.php">AUTORES</a>
+                <ul> 
+                    <li><a href="libros.php">LIBROS</a>
+                </ul>
+            </li>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div>
+    <center><h2>NOS PUEDE CONTACTAR AL SIGUIENTE TELÉFONO "940717714"
+        <p> Y SEGUINOS POR FACEBOOK <a href="https://www.facebook.com/aaenperu/">AA en Perú</a>
+    </h2>
+    </div>
+    <br>
+    <div align="left"> <p>
+      <img style="border:15px solid; color: blue;float:left; margin-right:20px;"src="imagen/atadura.jpg"width="450" height="263"  >
+     <h2> <br clear="right"><pr>ESTAMOS DISPONIBLES LAS 24 HORAS AL DÍA, <p>SI TIENES ALGÚN PROBLEMA ¡NO DUDES EN LLAMARNOS!!<p> LA UNIÓN HACE LA FUERZA.</h2>
+     <img style="border:15px solid; color: blue;float:left;margin-right:20px;"src="imagen/exi.jfif"width="350" height="150"  >
+     <img style="border:15px solid; color: blue;float:left;"src="imagen/alto.jpg"width="300" height="150"  >
+    </div>
+    </br>
+    </br>
+    </br>
+    <br><center><h3><a href="https://twitter.com/login?lang=es">Twitter:@guiliana_cw21</a><p> <a href="https://www.google.com/intl/es/gmail/about/">Correo:gvalercii@gmail.com</a></h3></center>
+    <br>
+    </body>
+</html>
