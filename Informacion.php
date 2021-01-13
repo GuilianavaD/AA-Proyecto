39680dcb7daa86ca30a81ccad27394ee8252d7e9
GuilianavaD <guilianavaler@gmail.com>
2021-01-12 19:47:54 -0500

    Update Informacion.php

--- Informacion.php
+++ Informacion.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+   
     <title>AA</title>
     </head>
     <body>
@@ -46,9 +47,14 @@
     <center><h1 style="color:#24c0e7">Información Sobre Alcohólicos Anónimos</h1>
     <br>
     <p>
-    <h2>Esta información es tanto para gente que pueda tener un 
+   
+    <h2 style="color:green">Esta información es tanto para gente que pueda tener un 
     problema <p>con la bebida como para aquellos que están en contacto<p> con 
     gente que tiene, o se sospecha que tiene, un problema.</h2>
     </p>
-    </body>
+    <br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/2ZjQcmRrGcY"
+         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+          gyroscope; picture-in-picture" allowfullscreen></iframe>
+     </body>
 </html>
