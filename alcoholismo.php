fac2bae688b94965aeefb3e1d0c146cc22c9b016
GuilianavaD <guilianavaler@gmail.com>
2021-01-14 17:54:31 -0500

    Update alcoholismo.php

--- alcoholismo.php
+++ alcoholismo.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="stylee.css">
     <title>AA</title>
     <style type="text/css">
     </style>
@@ -42,7 +43,105 @@
                 </ul>
             </li>
     </div>
+    </br>
     <br>
-    <!-- <h1>EL ALCOHOLISMO</h1> -->
+    </br>
+    <center><h1>EL ALCOHOLISMO</h1></center>
+    <br>
+    <div id="cotenedor">
+      <div id="cuerpo"> 
+        <div id="lateral">
+        <h2 style="color:red";> El alcoholismo es la única
+        enfermedad por la cual:</h2> </br><img style="border:10px solid; color: blue;float:right; margin-right:10px;"src="imagen/aa.jpg"width="120" height="150"  >
+        <br>*Te multan</p> 
+        <br>*Te llevan a la cárcel</p>
+        <br>*Pierdes el empleo</p>
+        <br>*Se desintegra tu familia</br>
+        <br>
+        <br>
+        </br>
+        <h4 style="color:red";>El alcoholismo también
+        provoca:</h4> </br><img style="border:10px solid; color: blue;float:right; margin-right:10px;"src="imagen/bb.jpg"width="120" height="190"  >
+        <br>
+        *Desgaste financiero</p>
+        <br>*Violencia</p>
+        <br>*Accidentes</p>
+        <br>*Luto en la familia</p></br>
+        *Daño en la salud física</br>
+        <br><h2 style="color:red";>Signos de alarma </h2></br>
+        *Incapacidad de detenerse y/o abstenerse al
+        consumo de alcohol. </br><img style="border:10px solid; color: blue;float:right; margin-right:10px;"src="imagen/Alcoholis.jpg"width="320" height="140"  >
+        </div>
+        <div id="otrolado"> 
+        <h2 style="color:red";>
+        ¿Qué es el alcoholismo?</h2></br>
+        El alcoholismo es una enfermedad que consiste
+        en padecer una fuerte necesidad de ingerir
+        sustancias que contenen alcohol, de forma que
+        existe una dependencia física del mismo,
+        manifestada a través de determinados síntomas
+        de abstinencia cuando se suspende su consumo.</p><br>
+        El alcohólico no tiene control sobre los límites de
+        su consumo, y suele ir elevando a lo largo del
+        tiempo su grado de tolerancia al alcohol.<p><br><br>
+        <img style="border:10px solid; color: blue;float:center; margin-left:40px;"src="imagen/bas.jpg"width="300" height="150"  >
+        <center><h2 style="color:red";>Causas del Alcoholismo</h2><br></center>
+        Hasta el momento no existe una causa común
+        conocida de esta adicción, aunque varios factores
+        pueden desempeñar un papel importante en su
+        desarrollo y las evidencias muestran que quien
+        tiene un padre o una madre con alcoholismo,
+        tiene mayor probabilidad de adquirir está
+        enfermedad, una puede ser el estrés o los
+        problemas que tiene la persona que lo ingiere.</p><br>
+        Eso puede deberse, más que al entorno social,
+        familiar o campañas publicitarias, a la presencia
+        de ciertos genes que podrían aumentar el riesgo
+        de alcoholismo.</p>
+        Algunos otros factores asociados a este
+        padecimiento, son la necesidad de aliviar la
+        ansiedad, conflicto en relaciones interpersonales,
+        depresión, baja autoestima, facilidad para
+        conseguir el alcohol y aceptación social del
+        consumo de alcohol. 
+        </div> 
+        <div id="principal"> 
+        <h2 style="color:red";>¿Quienes están en riesgo por
+        consumo de alcohol?</h2></br> 
+        La edad, el sexo y otras características
+        biológicas del consumidor determinan los
+        distintos grados de riesgo, también entran en
+        juego el grado de exposición a las bebidas
+        alcohólicas (cantidad, frecuencia y duración del
+        consumo) y las circunstancias en que se
+        produce la ingestión. <p>En las Embarazadas, el
+        consumo de alcohol puede provocar el
+        síndrome alcohólico fetal , que es un trastorno
+        permanente provocado por la exposición del
+        embrión y del feto al alcohol, ingerido por la
+        madre durante el periodo de gestación, por lo
+        que se arriesga a dar a luz a un bebé con
+        deficiencias mentales y físicas para el resto de
+        su vida. </p>
+        <img style="border:10px solid; color: blue;float:center; margin-left:30px;"src="imagen/aler.jpg"width="250" height="140"  >
+        <h2 style="color:red";>Existen 2 formas de alcoholismo:</h2></br>
+        •Alcoholismo Agudo: Llamado también
+        embriaguez o ebriedad. Es transitorio y
+        consiste en una crisis pasajera que va desde la
+        euforia a la tristeza, hasta la perdida de la
+        razón, mareos, nauseas y vómitos.</p><br>
+        •Alcoholismo crónico: Se produce por el
+        consumo habitual de moderadas dosis de
+        alcohol. El alcohólico presenta trastorno del
+        carácter (desconfianza, irritabilidad, con
+        periodos depresivos que pueden llevarlo al
+        suicidio en algunos casos extremos). Todos los
+        órganos resultan afectados, sobre todo el
+        sistema nervioso. 
+        </div> 
+    
+       </div>
+
+    </div>
     </body>
 </html>
