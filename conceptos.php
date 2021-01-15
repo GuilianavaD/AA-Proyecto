d0c55d79dcf16e8f071bcfb15f4eaa24db0fefb1
GuilianavaD <guilianavaler@gmail.com>
2021-01-15 08:43:47 -0500

    Update conceptos.php

--- conceptos.php
+++ conceptos.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.contacto.css">
     <title>AA</title>
     </head>
     <body>
@@ -43,7 +44,36 @@
     </br>
     </br>
     <br>
-    <!-- <center><h1>Conceptos que se tienen que saber del Alcoholismo </h1></center> -->
+  <body>
+    <div id="contenedor">
+      <div id="cabecera">
+      <center><p>Sítomas que se tienen que saber del Alcoholismo </p></center>
+      </div>
+      <div id="menu">
+          <img style="border:15px solid; color: blue;"src="imagen/alco.jpg"aling="left" width="120px" height="180" alt="" >
+          <img style="border:15px solid; color: blue;"src="imagen/alll.jfif"aling="left" width="120px" height="170" alt="" >
+          <img style="border:15px solid; color: blue;"src="imagen/bas.jpg"aling="left" width="120px" height="120" alt="" >
+          <img style="border:15px solid; color: blue;"src="imagen/alto.jpg"aling="left" width="120px" height="90" alt="" >
+      </div>
+      <div id="contenido">
+      <h1>SÍNTOMAS</h1><br>
+        <p>*Incapacidad de mantenerse sin consumir alcohol.<br><br>
+        *Promesas incumplidas de controlarse o abstenerse.<br><br>
+        *Descuido en la higiene personal y la alimentación.<br><br>
+        *Trastornos del sueño.<br><br>
+        *Ausencia, errores y retrasos recurrentes en el trabajo.<br><br>
+        *Angustia o presión en el pecho por las mañanas.<br><br>
+        *Mentir sobre la cantidad ingerida o esconder botellas.<br><br>
+        *Pérdida de tiempo y/o dinero en planear el consumo, consumiendo o por la resaca.<br><br>
+        *Beber solo o juntarse con personas con las cuales el único vínculo es la bebida.<br><br>
+        *Tener algún tipo de consecuencia por el consumo de alcohol y no ser capaz de controlarse.<br><br>
+        *Sentimientos de culpabilidad el día siguiente.<br><br>
+        *Lagunas mentales.<br><br>
+        *Ponerse a la defensiva al hablar sobre el problema.<br><br>
+        *Incapacidad para divertirse sin la presencia del alcohol.<br><br>
+        *Episodios de cambio de personalidad bajo los efectos del alcohol.<br><br></p>
+      </div>
+    </div>
     
     </body>
 </html>
