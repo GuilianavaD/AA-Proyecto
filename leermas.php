df1cbf73aeb7336629f0231279bcde08f6e59408
GuilianavaD <guilianavaler@gmail.com>
2021-01-17 18:47:53 -0500

    Create leermas.php

---
+++ leermas.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA</title>
+    <style type="text/css">
+    #titulo{
+        background-color:#08E3E1;
+        width:auto;
+    }
+    #contendor {
+        background-color:#D8FBFA;
+        float:left;
+        height:auto;
+        padding:10px;
+        margin:0 auto;
+        width:auto;
+      }
+    p{
+        background-color: rgb(188, 187, 243); 
+        text-align: justify; 
+    }
+    </style>
+    </head>
+    <body> <center>
+ <div id="titulo">       
+<h1>Diez consejos para evitar el alcoholismo</h1><a href="consejos.php"><input type='submit' value='regresar'></a></div></center>
+<div id="contendor">
+    <p>En el apartado de las adicciones, el alcoholismo es una de las más habituales. Puede afectar 
+    a cualquier persona, sea cual sea su estatus en la sociedad. Los adictos al alcohol corren riesgo de sufrir 
+    graves problemas de salud, pero además, también perjudican a los que les rodean. Familiares, amigos y compañeros sufren
+     también las consecuencias de este hábito tan nocivo. Es por eso que te ofrecemos diez consejos para dejar el alcohol.</p>
+<img src="imagen/alcoholicos1.jpg"aling="center" width="320px" height="180" alt="" >
+ 
+<h1>1º Darse cuenta de que existe el problema</h1>
+<p>Parecerá una tontería, pero el principal problema de un adicto es no reconocer su adicción. Muchos piensan que
+ lo dejarán cuando quieran. Otros simplemente evitan cuestionarse sus hábitos. Sin embargo el primer paso para dejar de 
+ beber es ser consciente del alcance del problema. Asumir que existe una dependencia y que es necesario abandonarla cuanto antes.
+  Y reconocer además que será imprescindible contar con ayuda. El alcoholismo es algo muy serio que debe ser tratado con rigor.</p>
+<h1>2º Ponerse en manos de profesionales cualificados</h1>
+<p>Para abandonar la adicción a las drogas o el alcohol, es necesario apoyarse en mucha gente.
+ No puede faltar un equipo médico que asesore al alcohólico, sometiéndolo a rigurosos controles, no sólo para 
+ controlar que no haya recaídas, sino para corroborar que su organismo esté en el mejor estado posible.
+  Quizá sea necesario recurrir a determinada medicación, pero siempre bajo prescripción médica. Además, 
+  el equipo médico también debe tomar parte en la parte psicológica del paciente.</p>
+
+<h1>3º Buscar apoyo de la gente cercana</h1>
+<p>Si un alcohólico no busca el apoyo de otra gente, tendrá prácticamente garantizada
+ la recaída. Los familiares y amigos más cercanos deben involucrarse, prestando apoyo emocional siempre
+  que el enfermo lo necesite. El alcohol causa, entre muchas otras cosas, depresión. Por eso es importante buscar
+   la complicidad y el cariño de los allegados. </p>
+<h1>4º Hacer deporte y comer sano</h1>
+<p>El alcoholismo provoca la pérdida de algunos nutrientes indispensables para nuestro organismo.
+ Por tanto es muy importante seguir una dieta equilibrada que  aporte todo aquello que falta. Hay 
+ dietas especiales para personas con problemas de alcoholismo. Lo mejor es preguntarle al equipo médico 
+ que vaya a realizar el seguimiento del paciente. Además, hacer deporte servirá como gran ayuda para superar 
+ cualquier adicción. Ocupará parte del tiempo del paciente, brindándole un bienestar tanto mental como físico.</p>
+<img src="imagen/hacer-deporte2.jpg"aling="center" width="320px" height="180" alt="" >
+ 
+
+<h1>5º Acudir a asociaciones</h1>
+<p>Hay asociaciones y programas especiales para personas con todo tipo de adicciones,
+ya sean de pago o de carácter gratuito. Ahí se puede acceder a charlas, reuniones y todo tipo de actividades,
+ con más personas con el mismo problema, lo cual servirá de ayuda para, en conjunto, unir fuerzas y luchar por 
+ dejar definitivamente el alcohol. Para contactar con estas asociaciones lo más fácil es preguntar a nuestro médico
+  o llamar al ayuntamiento del lugar de residencia, donde con mucho gusto nos pondrán en contacto con ellos.</p>
+<h1>6º Tolerancia cero</h1>
+<p>Algo que debe tener claro un alcohólico es que una vez que empiece su proceso de recuperación,
+ el alcohol debe desaparecer para siempre de su vida. No vale aquello de “es una ocasión especial”,
+  o “sólo un trago”. Ni en fiestas, ni en ningún otro evento. Una mísera gota puede acabar con un trabajo titánico. 
+  Hay que ser conscientes de que el alcohol es innecesario y perjudicial. Se vive mejor sin él. Pero eso es algo que 
+  el alcohólico sólo sabrá cuando consiga superar su hábito de alcoholismo.</p>
+
+ 
+
+<h1>7º Evitar malas compañías y lugares</h1>
+<p>Por malas compañías no nos referimos a gente mala. Sino gente que haga peligrar nuestro 
+propósito de dejar el alcohol. Llega el momento en que lo que importa es la propia vida de uno, aparcando la
+ relación con según qué personas. Es una situación en la que hay que salvarse. Cuestión de prioridades. Si por ejemplo,
+  tenemos un compañero de borracheras, lo mejor es evitarle. O en todo caso, convencerle y buscar la ayuda mutua para 
+  salir juntos del problema. Y si hay lugares en los cuales el alcohólico consumía habitualmente, también hay que tratar
+   de evitar acudir a ellos.</p>
+<img src="imagen/bares3.jpg"aling="center" width="320px" height="180" alt="" > 
+
+<h1>8º Buscar hobbys</h1>
+<p>Un alcohólico debe ocupar su vida con cosas que le llenen. Como ya hemos comentado, el deporte
+ es una excelente válvula de escape. Estar con los seres queridos para recuperar el tiempo perdido 
+ con ellos también. Pero hay otros hobbys como las manualidades, los viajes, la lectura, etc… que también
+  serán un buen apoyo para continuar con una vida plena y sana.</p>
+
+<h1>9º Ánimo, autoestima, convencimiento</h1>
+<p>Otra de las principales premisas para tener éxito en dejar de beber, es estar lo más
+ llenos posible de ánimo. Ser conscientes de que si otros han podido, nosotros también. En realidad es
+  algo simple. A nadie le gusta estar inmerso en un estado de amargura, perjudicándose a sí mismo y los que
+   le rodean. Por lo tanto, qué ganamos bebiendo? Nada… Es una pérdida absoluta de tiempo, dinero, respeto, etc…
+    Los problemas existen, pero el alcohol nunca los resuelve. Los empeora. Además hay que olvidarse de los prejuicios.
+     Asumir que hay un problema sin que importe el qué dirán los demás. Porque recuperar la vida que uno tenía antes de
+      caer en las garras del alcohol, es posible. Y eso es lo que verdaderamente importa.</p>
+
+<h1>10º Tratamientos naturales</h1>
+<p>El tratamiento médico puede ir acompañado de otros tratamientos de carácter natural, como pueden 
+ser la acupuntura, la hipnoterapia, los masajes o la meditación. Por sí solos, estos tratamientos no servirán 
+para abandonar la adicción, pero si pueden ser una excelente ayuda complementaria.</p>
+<img src="imagen/masajes4.jpg"aling="center" width="320px" height="180" alt="" >
+</div>
+
+    </body>
+</html>
