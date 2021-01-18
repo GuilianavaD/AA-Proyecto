25d1cc48ee12cd9aeb331a785def92f10d7b6090
GuilianavaD <guilianavaler@gmail.com>
2021-01-18 18:31:18 -0500

    Update recomendacion.php

--- recomendacion.php
+++ recomendacion.php
@@ -6,6 +6,25 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>AA</title>
     <style type="text/css">
+    h2{
+        margin:8px;
+        padding:15px;
+        text-align: center; 
+        background-color: cyan;  
+        border: PowderBlue 5px double;
+        border-top-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+    }
+    h3{
+        margin:8px;
+        padding:25px;
+        text-align: justify; 
+        background-color: #ddd;  
+        border: PowderBlue 5px double;
+        border-top-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+    }
+    }
     </style>
     </head>
     <body>
@@ -43,5 +62,24 @@
             </li>
     </div>
     <br>
-    </body>
+    <br>
+    <br>
+    <h2>Sigue estás recomendación utilizadas en Alcohólicos Anónimos los doces pasos</h2>
+    <h3>
+   1.- Admitimos que éramos impotentes ante el alcohol, que nuestras vidas se habían vuelto ingobernables.</p><br>
+   2.- Llegamos a creer que un Poder superior a nosotros mismos podría devolvernos el sano juicio.</p><br>
+   3.- Decidimos poner nuestras voluntades y nuestras vidas al cuidado de Dios, como nosotros lo concebimos.</p><br>
+   4.- Sin miedo hicimos un minucioso inventario moral de nosotros mismos.</p><br>
+   5.- Admitimos ante Dios, ante nosotros mismos, y ante otro ser humano, la naturaleza exacta de nuestros defectos.</p><br>
+   6.- Estuvimos enteramente dispuestos a dejar que Dios nos liberase de nuestros defectos.</p><br>
+   7.- Humildemente le pedimos que nos liberase de nuestros defectos.</p><br>
+   8.- Hicimos una lista de todas aquellas personas a quienes habíamos ofendido y estuvimos dispuestos a reparar el daño que les causamos.</p><br>
+   9.- Reparamos directamente a cuantos nos fue posible el daño causado, excepto cuando el hacerlo implicaba perjuicio para ellos o para otros.</p><br>
+   10.- Continuamos haciendo nuestro inventario personal y cuando nos equivocábamos lo admitíamos inmediatamente.</p><br>
+   11.- Buscamos a través de la oración y la meditación mejorar nuestro contacto consciente con Dios, como nosotros lo
+    concebimos, pidiéndole solamente que nos dejase conocer su voluntad para con nosotros y nos diese la fortaleza para cumplirla.</p><br>
+   12.- Habiendo obtenido un despertar espiritual como resultado de estos pasos, tratamos de llevar el mensaje a los alcohólicos 
+   y de practicar estos principios en todos nuestros asuntos.</p><br>
+</h3>
+</body>
 </html>
