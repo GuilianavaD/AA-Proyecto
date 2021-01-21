925ee0b084065a5b2f0b530d69682df69579648d
GuilianavaD <guilianavaler@gmail.com>
2021-01-20 22:15:47 -0500

    Create leercarloscautemoc.php

---
+++ leercarloscautemoc.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA</title>
+    <style type="text/css">
+    body{
+        margin-left: 40px;
+        margin-right: 40px;
+        margin-top: 40px;
+        margin-bottom: 30px;
+    }
+    table{
+        background-color:#ddd;
+        text-align: justify; 
+    }
+    #cabe {
+        color:#ff9;
+        background-color:#000;
+        padding:10px;
+      }
+    #pie {
+        background-color:#000;
+        padding:10px;
+      }
+    </style>
+    </head>
+    <body>
+        <div id="cabe">
+    <h1>&nbsp;&nbsp;&nbsp;BIOGRAFÍA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
+    <table><tr><th>
+    <h1> Carlos Cuauhtémoc Sánchez</h1> 
+   (México, 1964) Escritor mexicano. Licenciado en Ingeniería y catedrático de Dirección de Empresas y Ciencias Exactas, por sus libros
+ sobre la familia, el perdón, la fe y la formación del carácter ha sido definido como uno de los guías éticos de moda para la juventud
+  y uno de los más autorizados filósofos de la superación y el liderazgo en Latinoamérica. Para sus detractores, sin embargo, es sólo 
+  otro escritor de libros de autoayuda, un moralista conservador cuyo éxito se fundamenta en las carencias educacionales de la sociedad 
+  moderna.<br>
+ <center> <img style="border-radius: 20px;"src="imagen/sanchez_carlos_cuauhtemoc.jpg"aling="left" width="300px" height="200" alt="" >
+  <h4>Carlos Cuauhtémoc Sánchez</h4><center>
+  Pese a ello, sus libros han encabezado las listas de best sellers de la literatura latinoamericana, habiendo sido algunos de ellos traducidos 
+  al inglés, al francés y al portugués.A raíz de su popularidad, Carlos Cuauhtémoc Sánchez ha sido también colaborador en diversos foros de radio 
+  y televisión como especialista en el área de formación humana. Obtuvo el Premio Nacional de las Mentes Creativas otorgado por la Dirección General 
+  del Derecho de Autor y el Premio Nacional de la Juventud en literatura, que le fue entregado por el presidente Miguel de la Madrid. Ha impartido
+   también conferencias ante numerosos auditorios del mundo hispano.
+<br><br>
+Carlos Cuauhtémoc Sánchez inició su trayectoria con Un grito desesperado (1992), áspera novela sobre los conflictos y el entorno familiar y 
+escolar de un joven inadaptado. Cosechó a continuación un arrollador éxito de público con Juventud en éxtasis (1993), obra de la que se venderían
+ más de dos millones de ejemplares a partir de su publicación y que, todavía en 2005, aparecía como el libro más leído en la Encuesta Nacional
+  de Lectura; en esta segunda novela abordó la sexualidad partiendo de la figura de un estudiante universitario cuyas actitudes sobre el sexo, 
+  el amor y las relaciones prematrimoniales quedan cuestionadas al descubrir que padece una enfermedad de transmisión sexual. El autor abundó 
+  en esta línea moralista con Juventud en éxtasis 2 (1997).
+<br><br>
+A este éxito siguieron diversos libros dedicados a los más inmediatos conflictos y problemas que acucian a la sociedad de nuestros días:
+ las novelas La última oportunidad (1994) y Contraveneno (2000) se centran en las crisis matrimoniales y el divorcio, y Volar sobre el pantano 
+ (1995) en las lacras sociales que acechan a los jóvenes. De tono más personal es La fuerza de Sheccid (1996), protagonizada por un adolescente 
+ que encuentra en el amor la energía necesaria para sobreponerse a las dificultades; esta novela sería reelaborada y ampliada en Los ojos de mi 
+ princesa (2004) y Los ojos de mi princesa 2 (2012). Casi todas estas obras alcanzaron impresionantes cifras de ventas, aunque no puede obviarse 
+ que, por su perfil moralizante y por su capacidad para conectar con las más vivas preocupaciones de los jóvenes, muchas de ellas fueron seleccionadas 
+ como lecturas obligatorias en los centros de enseñanza.
+<br><br>
+<center> <img style="border-radius: 20px;"src="imagen/carloos.jfif"aling="left" width="300px" height="200" alt="" >  <h4>Carlos Cuauhtémoc Sánchez</h4><center></center>
+Las novelas Dirigentes del mundo futuro (1999) y la tetralogía formada por Sangre de Campeón (2001), Sangre de Campeón Sin cadenas (2002), 
+Sangre de Campeón Invencible (2003), y Sangre de campeón: En pie de guerra (2006) reflejan sus preocupaciones y propuestas pedagógicas. 
+El misterio de Gaia (2004) constituye, con su mezcla de géneros, una audaz y cuestionada innovación narrativa situada en la fantástica
+ isla de Gaia, parábola de nuestro mundo. En Mujeres de conquista (2005) asistimos al replanteamiento por parte del protagonista, sepultado
+  durante varios días tras un terremoto, de su visión de la mujer.
+<br><br>
+Después de esta novela, Carlos Cuauhtémoc Sánchez alternó la ficción narrativa con mensaje que había caracterizado su 
+producción con ensayos sobre crecimiento personal en los que trató temas diversos, como la productividad, la pareja, el amor y 
+la autorrealización: Te desafío a prosperar (2006), Te desafío a disfrutar el amor (2007), ¿Free Sex? La pregunta de hoy (2008), 
+Ser feliz es la meta (2011) y Tiempo de ganar (2013), aunque en algunos, como Luz en la tormenta (2009), la exposición se combina con historias ficticias.
+Entre sus últimas novelas cabe citar Los fantasmas del espejo (2008), sobre una chica anoréxica; Decisión crucial (2010); El virus (2010), 
+historia de un hombre que, tras contraer una extraña enfermedad, indaga en el sentido del sufrimiento; El feo (2010); Mientras respire (2014),
+ sobre tres mujeres al borde del suicidio, y Si quieres casarte con mi hija, debemos hablar (2015), en que un padre se dirige al novio de su hija.<br>
+<br>
+Casado desde 1986 con Ivonne, que le dio tres hijos, Carlos Cuauhtémoc Sánchez no se arredra ante quienes denuestan sus posturas conservadoras y
+ sus escasas aptitudes literarias; en una entrevista concedida en marzo de 2011 aceptaba las críticas («los que saben enseñar, aleccionar y 
+ guiar en valores, por lo regular no han desarrollado capacidades literarias»), pero reivindicaba a la vez el valor de su obra: «Mis novelas 
+ están basadas en hechos reales y plasman posibles soluciones a diversos problemas [...] Escribo novelas en las que al mismo tiempo transmito los valores en los que creo.»
+</table></tr></th>
+<div id="pie">
+    hola
+    </div>
+    </body>
+</html>
