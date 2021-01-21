3b7919b3d55a09e8b251a294aa49563e261f7ea2
GuilianavaD <guilianavaler@gmail.com>
2021-01-21 13:14:51 -0500

    Create libros.php

---
+++ libros.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>AA</title>
+    <style type="text/css">
+    body{
+        margin-left: 40px;
+        margin-right: 40px;
+    }
+    table{
+        background-color:#ddd;
+        float:center;
+        text-align: justify; 
+    }
+    #cabe {
+        color:#ff9;
+        background-color:#000;
+        padding:10px;
+        text-align: justify; 
+      }
+    #pie {
+        background-color:#000;
+        padding:10px;
+      }
+    </style>
+    </head>
+    <body>
+    <div id="header">
+        <ul class="nav">
+            <li><a href="Inicio.php">INICIO</a>
+                <ul>
+                    <li><a href="Informacion.php">INFORMACIÓN</a>
+                
+                    </li>
+                    <li><a href="contacto.php">CONTACTOS</a>
+                    
+                    </li>
+                    <li><a href="misivisi.php">MISIÓN Y VISIÓN</a>
+                    </li>
+                    <li><a href="index.html">Cerrar Sesión</a>
+                </ul>
+            </li>
+            <li><a href="conceptos.php">SÍNTOMAS</a>
+                <ul>
+                <li><a href="alcoholismo.php">EL ALCOHOLISMO</a>
+                </li>
+                </ul>
+            </li>
+
+            <li><a href="consejos.php">CONSEJOS</a>
+                <ul> 
+                    <li><a href="recomendacion.php">RECOMENDACIONES</a>
+                </ul>
+            </li>
+            <li><a href="autores.php">AUTORES</a>
+                <ul> 
+                    <li><a href="libros.php">LIBROS</a>
+                </ul>
+            </li>
+    </div><br><br><br>
+    <div id="cabe">
+    <h1>SI DESEA ALGÚN LIBRO DE ESTÁ PÁGINA LO PUEDE DESCARGAR &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
+    <table><tr><th>
+    <br>&nbsp;<img style="border-radius: 20px;"src="imagen/no_rendir.jfif" aling="right" width="340px" height="200" alt="" >&nbsp;<br clear="right"> 
+    <a href="https://pnsd.sanidad.gob.es/profesionales/publicaciones/catalogo/bibliotecaDigital/publicaciones/pdf/1_Socigrupospsicoactivos2000.pdf">libro Alcohol - Plan Nacional sobre Drogas</a><br><p>
+    <br>
+    <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Flookaside.fbsbx.com%2Ffile%2FLa_estepa_de_los_malditos.pdf%3Ftoken%3DAWwLT00rHnmVe_60zyp21PfZM6WX6Ds12lvT6LcAmTlOHzS8ttOFFMzeVYCtwJmxBGzZWBchc5a3jKpqIU_N-EccFsWRcg_tLcefoib72Uj0VWCMmJsDgAHzz3YLQt78GcS76qe8VMFD1aQ2xpn63VZYAa61aCSDe0x4NPa4RmLQ5d0SpyBbjJ3IjbJDl2To-oklUY2FpKDxvqFQJJ_XZfO77hw1jsxhJbWCQ7UidK3bT1YuStDsfhUCb-Q7X7tq2T1Bnfa4bI27yo05q1CK9DfxXEcMkYQka5l_7RuALevlBXhlqZ7QHy-y3z93Slj9amUe6cGnToKJIwpwSWg4Cc8dXT8sZmyDzWchRsF9wWp_W2ENqoLZv0FGGUYwygML1eIh6RougO_nf5PlkcOPGWHMX4fQFrDmWCS8e7zg2tqkPBmlT3QK71QmH4vffl8ou_joBuaD6LSAGA075DDTJISpKJ8vWZA5KD85pQ89HvOwC4ODTjAz8RdKstlXjiXI72l28ky2ZLHIKz_qD61TBFvQZFxjpNTV2mhQPEU2GsZzNneRg5nAbDHo74IKaoTn6BN_9W8rOm4pIVgOxRXLojqtWtFcVftBxYnMqTK7UNjieMNslWaSwt_j1UZX8a9D-uU&h=AT2_odXjnjodCSmogDcAVqYO9Tq_oiwzCpbmafahN1G7sKVZ9cVUXie46OkjJ-63s7v2kQ5P5lUGp4jlg2kRgnqsFEW4d-2fQ9NZ29oYsp7L2sYrlECd_QaJuy4XWUX-p3U3UJMuRC9khWbJ&__tn__=H-R&c[0]=AT0p8LDnHp-UALIZaz37Vt2y1oNBBNa3wSG1vlqb-KZB211azq8UAk1v4iB4zljYG2c8nZplKwinZL3XbSNrDyZoxT1bZvfgh19RCbHnLJgw-OGWG14DtDOHuqGiG7LS-IoA-afGxigbpF1DA6b5oH-QJ5qk5peBWgIxiftecsHA3Up0">La_estepa_de_los_malditos.pdf</a><br><p>
+    <br>
+    <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Flookaside.fbsbx.com%2Ffile%2FSI%2520DOMINO%2520MI%2520MENTE%2520CONTROLO%2520MI%2520VIDA%2520-%2520JAVIER%2520MU%25C3%2591OZ.pdf%3Ftoken%3DAWyan-aUVqPZDCe3lxQB0LyhEY-z1gdOtI7rYY8Dun52DZ7kSc-Mj3brfhTCm2JHPNMNFmbc9cRDqT0z-J_iESC6pdSV0QOL3MavaygqcPXeayUqqhdjz1EgIKEW9PVTyD4BEGUs422zmQ6MC0lHv-Wm14hpbCeQ3WG9YCM5KXebrEpCTpIuQVP8B7S4ZJCfZe_QKqZTrUOniAKzhNx-ysaBKeNNeqKecp3twzWIpUy0J7Pf2a9wJEtcdX8jpafuji9wTwCufkr8LtWILaMg7MVjIUkVe7xePc7xQXAmnITArj5rf2Y4BT37P2P--NMsC3-qhUbNqkzTZ4JbSLnVYWA3T08lN70gM9axELEhSgVG6_ZIagVPpQi5qwU1sfpve3vQDTwS2cHrpJTPIYaZKCF6t8Kcze66bBTumbAvEVCnwLo69jrgvjuvaz58y4oeiyMkdceU5XyQ87qjmRSQ-RXgtivWnh2EmZtbGcBUTd0D1HM7uoumrC2J5AqlXylJgR3Me1kThRuOloQxojDvZuFyveGf-TcRfxYVkyEajjTyH-_3a-HTD20ntLnqGJPaQBJIfR1PRtydOva5XIDLvYvhrfXPFTTLPeZLKyuhVFY0gMn5OrZQc-TDTHefWuwgGR8&h=AT3h-sdZrW8MhOVNDwl6vQC5xxrly5GMl4mdszd2nZ73zfHTL_M7kb4t43GlSWM6qkeI7pMoFlMMkoioHFm9ODuF6TQ8DN6F4DWYyhSpvOLRMMKu0Ku_O4BvXbeCxfs5OA3-Ppc-GbFqkr0f&__tn__=H-R&c[0]=AT0ntE5_CDiqEEouO6c5uJnPxD1gigUpZ1rmqcIusU-w3fQt1IWdVELkcjln_PnSQ7g0FkImQK-A8ZLBeb5Mtc9iDid0RtgsPXZKvq8rUenHviqI1fdmlWOAhlL7vDEc-UILip1lM6Go5WPw0HGZoABEROoz6KWckgasVY9kaPlPiCGA">SI DOMINO MI MENTE CONTROLO MI VIDA...pdf</a><br><p>
+    <br>
+    <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Flookaside.fbsbx.com%2Ffile%2FEL_IMPERIO_INVISIBLE.pdf%3Ftoken%3DAWwTnH86VlOpHQKK9eirF3OskckkYzW9BjVlDx5oXsF207V9NbBJYmHQRx4WBu8jGYdSAgZrfX95eZSVLXWhfDaVmzmCYY-gyjZ6FS9WlSqpe-DvjRvdieCfeaijhrB0k9ncHEjn7eFEIOKe95xAzkZdAtEotwnALVRCIwWs111zt3s4u4krpBQds2mxVsUguc7_g5sLiyNK6vhDw6CYc82M0ITLS2HRZYdU9vIJ42CNqMFbe1rPg9L790ln0GdfIMhCvy-gJUrZjHWxp3VSLud62hn0dH581937aHvgWPJ5GvGnqD09srv_RcgFJ9zt-fAQcFwUFlLano1j7_JYER1Z7GUa7kbrHVzY27Ou_QGq-mshUEbMz8Vd0Aciho86m8YI-o_gz3QiECCu52aQ6hR7WETNFrz6mhqHq5omNTN9m6fcAxv-5WE56ex7juJBgCALBYS8_LO7nH4FNLIqj7G1Buf87deuVn2axdqjEdfqQ0ANhViiFhoB5as5Jk9KYmAvokzXuDwxeN8U8IwnV5eoJR_QZteK7DINHkip62Iv0wDYPwG19vP1BAJqKLKLrvPbj_bU1IbLAqdRW4Zd7k0qku79EuGqhcNdoJuP3a1QZJ8w7nnpYOIvA7EFubBQ3e4&h=AT0RWg6U96N60_dODbjUq0UAH64PRqMt1E9j11ih-9m3TbJw_MMIs5_LFvsbI80BBXOwgFhYWx8dyAivMgQPrz8u09iBwAf8Rj68Z0sT0kiYm9uyWbDp8rc_MxEiasYwToPCutAMiHnXppob&__tn__=H-R&c[0]=AT37_1DiVnMYxMLFTU7IpC3Mcjj6ARJ2rna3Ce9wDx59yQfiSpAkLrvWTLBanfWGxs0zu2CiO77OjJvjs4nfxR-hsT4iUKD5xmT-bSOvvOJep6K61Ik3b38WhPev5gYS28hj1b8B9kbyww2qrkatDdsSVpOsvH9AUcVIS8wtq_HXt_7S" >EL_IMPERIO_INVISIBLE.pdf</a><br><p>
+    <br>
+    <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Flookaside.fbsbx.com%2Ffile%2FTUS%2520ZONAS%2520MAGICAS%2520-%2520WAYNE%2520W.%2520DYER%2520-%25201110%2520PAGINAS.pdf%3Ftoken%3DAWzP3yvVWTFm9JgQuOmk9u2xPyQiGzBvOHbRuBhL7uPSQXIBYHMSDqbwxZWZ3lKqK21cq5R7mZquwVQGyAs2j-JviFjt1et-P-Nm03izuUF-iSp7t9j43nFLKO7tGtKH0Sn9Iz9LJsnLA-igNcqlteMZod79VVdwAgu0Gm5B5bWwCtmtG78phwWF4pY4BkuGarkYHkPZdymUsiWFLoOjj6zE7LYNYtzGe8IGo2lkiuC0o5Tsoq5edw4yeV0zmjQ-zMRnupnu-Bm0IAM4eJU7X_uC72NRA1mxwENO7NAX39X78fZQLpu8gIDnz40aLwe1iowlr08FSm9K0eJdUpLjP7tJUpzgLCvm-DWjjyMMPW_GPRSfUpPR6ZQz89vb5aQX7swQrodM0y_PfpXWPgPBz2F3OKYMYgwMc5mHU8_cX_ZqQ_2BV5WsIEar9xv8nvv1q7MgFD5z3z8QDPG1YtI3l27ci-RBbvV9PULVgyHdJLMlYpPClot18QEDZfGQkMKnHBMUov05_Bn_-0Npni2J_9Os91YazKlkgGObMzm-49y9MleDlRSUhLHNvNqBQiAx-HpMzrzqbYTqPCTnsY2CtmNcj60N4I1AoUNDosKcY7E6aflJpq2vScNBnF8XX-H0uC8&h=AT3-KtJa0U6bE_d3NebmTyn1bKcFE6zxz6rEIqLzrRkwelT_zh31AiN11Q2I2Q1oI7E5ajVW4L2QzHHT3954Iqvd4MA-oaysZb_lsYHbsByxFdYDIMW5C9aJ0NHLKGkf5bDAmbM6GPe7cGUj&__tn__=H-R&c[0]=AT2yJ6aoNl9GodDy6HfGaOxA6PxDMbK368r2kiOWnErUwmcw62Gn3xCd8MaVIawQVIKessJG6hvuHTHfHZ5bjltzxBTKhb3WKnCuR0W5fvJ2dk5MrffDb3h_TsEfJ7jB8RxK3cgYIiwKtsrRxxuOyBKpyNamgxlChnunIYazt992-FW4">TUS ZONAS MAGICAS - WAYNE W. DYER</a><br><p>
+    <br>
+    <a href="https://pnsd.sanidad.gob.es/profesionales/publicaciones/catalogo/bibliotecaDigital/publicaciones/pdf/1_Socigrupospsicoactivos2000.pdf">libro Alcohol - Plan Nacional sobre Drogas</a><br><p>
+    <br>
+     </table></tr></th><br>
+<div id="pie">
+    hola
+    </div>
+    </body>
+</html>
