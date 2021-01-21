d8a94ef8aecc9025918687761cb62d250aa50d08
GuilianavaD <guilianavaler@gmail.com>
2021-01-20 22:16:05 -0500

    Create leerwillian.php

---
+++ leerwillian.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA</title>
+    <style type="text/css">
+    body{
+        margin-left: 40px;
+        margin-right: 40px;
+        margin-top: 40px;
+        margin-bottom: 30px;
+    }
+    table{
+        background-color:#ddd;
+        float:left;
+        text-align: justify; 
+    }
+    #cabe {
+        color:#ff9;
+        background-color:#000;
+        padding:10px;
+      }
+    #pie {
+        background-color:#000;
+        padding:10px;
+      }
+    </style>
+    </head>
+    <body>
+        <div id="cabe">
+    <h1>&nbsp;&nbsp;&nbsp;UN POCO DE SU BIOGRAFÍA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
+    <table><tr><th>
+    <h1>William Fergus Martin </h1>    <img style="border-radius: 20px;"src="imagen/willian_fergus_martin.jfif"aling="right" width="290px" height="180" alt="" ><br>William Fergus Martin es un autor independiente, orador y líder de talleres que ayuda a las
+    personas a empoderarse a sí mismas.<br> Él es escocés y está casado con una dama japonesa.
+    Ha viajado extensamente y su escritura está influenciada por una apreciación de los desafíos
+    comunes que enfrentan las personas en todas partes del mundo. <br>
+    William está feliz de escribir artículos a medida, dar charlas y hacer talleres, sobre todo en el
+    tema del perdón. <br>Él disfruta utilizando su experiencia anterior de la escritura y entrenamiento,
+    adquirido en el campo de la computación, para ayudar a la gente a aprender a perdonar.
+    <br>
+    </table></tr></th><br>
+<div id="pie">
+    hola
+    </div>
+    </body>
+</html>
