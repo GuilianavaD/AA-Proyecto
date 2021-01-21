e12a36402eeaf381f463cdc32d938c51e397bde8
GuilianavaD <guilianavaler@gmail.com>
2021-01-21 13:14:20 -0500

    Create leerbill.php

---
+++ leerbill.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA</title>
+    <style type="text/css">
+    body{
+        margin-left: 40px;
+        margin-right: 40px;
+        margin-top: 40px;
+        margin-bottom: 30px;
+    }
+    table{
+        background-color:#ddd;
+        float:left;
+        text-align: justify; 
+    }
+    #cabe {
+        color:#ff9;
+        background-color:#000;
+        padding:10px;
+      }
+    #pie {
+        background-color:#000;
+        padding:10px;
+      }
+    </style>
+    </head>
+    <body>
+        <div id="cabe">
+    <h1>&nbsp;&nbsp;&nbsp;BIOGRAFÍA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
+    <table><tr><th>
+     <center><h1>Bill W.</h1> 
+   <img style="border-radius: 20px;"src="imagen/bill-w-alcoholics-anonymous-header.jpg"aling="left" width="340px" height="200" alt="" ></center><p>
+     William Griffith Wilson (Dorset, Vermont; 26 de noviembre de 1895 – Miami, Florida; 24 de enero de 1971), también conocido
+      como Bill Wilson o Bill W., fue el fundador de la asociación Alcohólicos Anónimos junto con el Dr. Robert Smith (el Doctor Bob), 
+      una comunidad de personas que, en grupos de autoayuda, se recuperan de su problema común: el alcoholismo.<p>
+
+    Bill alcanzó la sobriedad el 11 de diciembre de 1934 y la mantuvo durante más de 36 años. Sin embargo este
+     éxito no fue ajeno a dificultades o episodios de conducta compulsiva o depresión. <p>
+     Bill W., cofundador de Alcohólicos Anónimos, escribió más de 150 artículos entre los años 1944 y 1970. Este libro contiene 
+   todos esos artículos en un volumen, incluidos sus primeros pensamientos sobre las Tradiciones, sus batallas contra
+    la depresión crónica y el orgullo espiritual, los recuerdos de una juerga durante toda la noche con Ebby y una vívida
+     descripción de cómo llegó a organizar los Pasos. (Habían solo seis en el primer borrador).<p>
+     En sus últimos años experimentó con otras posibles curas para el alcoholismo: el LSD, la niacina o vitamina B3 y la parapsicología para inducir 
+      un cambio espiritual.Bill murió de enfisema complicado por neumonía en 1971. <p>Su mujer, Lois Wilson (Lois W.),
+       fue la fundadora de Al-Anon, un grupo dedicado al auxilio de los amigos y familiares de alcohólicos. En 1999 Time
+        Magazine incluyó a Bill entre los veinte primeros del Time 100: Héroes e iconos que ejemplifican "coraje, autodominio,
+        exuberancia, habilidad sobrehumana y gracia maravillosa" en el siglo XX.<p>
+     </table></tr></th><br>
+<div id="pie">
+    hola
+    </div>
+    </body>
+</html>
