37244f159e77fc2a87d54451ce4fa3135a7afe68
GuilianavaD <guilianavaler@gmail.com>
2021-01-21 13:14:47 -0500

    Update leerwillian.php

--- leerwillian.php
+++ leerwillian.php
@@ -32,11 +32,11 @@
     <h1>&nbsp;&nbsp;&nbsp;UN POCO DE SU BIOGRAFÍA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
     <table><tr><th>
     <h1>William Fergus Martin </h1>    <img style="border-radius: 20px;"src="imagen/willian_fergus_martin.jfif"aling="right" width="290px" height="180" alt="" ><br>William Fergus Martin es un autor independiente, orador y líder de talleres que ayuda a las
-    personas a empoderarse a sí mismas.<br> Él es escocés y está casado con una dama japonesa.
+    personas a empoderarse a sí mismas.<br><p> Él es escocés y está casado con una dama japonesa.
     Ha viajado extensamente y su escritura está influenciada por una apreciación de los desafíos
-    comunes que enfrentan las personas en todas partes del mundo. <br>
+    comunes que enfrentan las personas en todas partes del mundo. <br><p>
     William está feliz de escribir artículos a medida, dar charlas y hacer talleres, sobre todo en el
-    tema del perdón. <br>Él disfruta utilizando su experiencia anterior de la escritura y entrenamiento,
+    tema del perdón. <br><p>Él disfruta utilizando su experiencia anterior de la escritura y entrenamiento,
     adquirido en el campo de la computación, para ayudar a la gente a aprender a perdonar.
     <br>
     </table></tr></th><br>
