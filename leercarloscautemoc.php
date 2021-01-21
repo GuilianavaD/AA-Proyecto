a6c6ae50ad68fa75326d33fababdb82b1c5d862d
GuilianavaD <guilianavaler@gmail.com>
2021-01-21 13:14:27 -0500

    Update leercarloscautemoc.php

--- leercarloscautemoc.php
+++ leercarloscautemoc.php
@@ -28,7 +28,7 @@
     </head>
     <body>
         <div id="cabe">
-    <h1>&nbsp;&nbsp;&nbsp;BIOGRAFÍA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
+    <h1>&nbsp;&nbsp;&nbsp;BIOGRAFÍA Y OBRAS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
     <table><tr><th>
     <h1> Carlos Cuauhtémoc Sánchez</h1> 
    (México, 1964) Escritor mexicano. Licenciado en Ingeniería y catedrático de Dirección de Empresas y Ciencias Exactas, por sus libros
@@ -37,10 +37,10 @@
   otro escritor de libros de autoayuda, un moralista conservador cuyo éxito se fundamenta en las carencias educacionales de la sociedad 
   moderna.<br>
  <center> <img style="border-radius: 20px;"src="imagen/sanchez_carlos_cuauhtemoc.jpg"aling="left" width="300px" height="200" alt="" >
-  <h4>Carlos Cuauhtémoc Sánchez</h4><center>
+  <h4>Carlos Cuauhtémoc Sánchez</h4></center>
   Pese a ello, sus libros han encabezado las listas de best sellers de la literatura latinoamericana, habiendo sido algunos de ellos traducidos 
   al inglés, al francés y al portugués.A raíz de su popularidad, Carlos Cuauhtémoc Sánchez ha sido también colaborador en diversos foros de radio 
-  y televisión como especialista en el área de formación humana. Obtuvo el Premio Nacional de las Mentes Creativas otorgado por la Dirección General 
+  y televisión como especialista en el área de formación humana.<p> Obtuvo el Premio Nacional de las Mentes Creativas otorgado por la Dirección General 
   del Derecho de Autor y el Premio Nacional de la Juventud en literatura, que le fue entregado por el presidente Miguel de la Madrid. Ha impartido
    también conferencias ante numerosos auditorios del mundo hispano.
 <br><br>
@@ -53,23 +53,23 @@ escolar de un joven inadaptado. Cosechó a continuación un arrollador éxito de
 <br><br>
 A este éxito siguieron diversos libros dedicados a los más inmediatos conflictos y problemas que acucian a la sociedad de nuestros días:
  las novelas La última oportunidad (1994) y Contraveneno (2000) se centran en las crisis matrimoniales y el divorcio, y Volar sobre el pantano 
- (1995) en las lacras sociales que acechan a los jóvenes. De tono más personal es La fuerza de Sheccid (1996), protagonizada por un adolescente 
+ (1995) en las lacras sociales que acechan a los jóvenes.<p> De tono más personal es La fuerza de Sheccid (1996), protagonizada por un adolescente 
  que encuentra en el amor la energía necesaria para sobreponerse a las dificultades; esta novela sería reelaborada y ampliada en Los ojos de mi 
  princesa (2004) y Los ojos de mi princesa 2 (2012). Casi todas estas obras alcanzaron impresionantes cifras de ventas, aunque no puede obviarse 
  que, por su perfil moralizante y por su capacidad para conectar con las más vivas preocupaciones de los jóvenes, muchas de ellas fueron seleccionadas 
  como lecturas obligatorias en los centros de enseñanza.
 <br><br>
-<center> <img style="border-radius: 20px;"src="imagen/carloos.jfif"aling="left" width="300px" height="200" alt="" >  <h4>Carlos Cuauhtémoc Sánchez</h4><center></center>
+<center> <img style="border-radius: 20px;"src="imagen/carloos.jfif"aling="left" width="300px" height="200" alt="" >  <h4>Carlos Cuauhtémoc Sánchez</h4></center>
 Las novelas Dirigentes del mundo futuro (1999) y la tetralogía formada por Sangre de Campeón (2001), Sangre de Campeón Sin cadenas (2002), 
 Sangre de Campeón Invencible (2003), y Sangre de campeón: En pie de guerra (2006) reflejan sus preocupaciones y propuestas pedagógicas. 
 El misterio de Gaia (2004) constituye, con su mezcla de géneros, una audaz y cuestionada innovación narrativa situada en la fantástica
- isla de Gaia, parábola de nuestro mundo. En Mujeres de conquista (2005) asistimos al replanteamiento por parte del protagonista, sepultado
+isla de Gaia, parábola de nuestro mundo. En Mujeres de conquista (2005) asistimos al replanteamiento por parte del protagonista, sepultado
   durante varios días tras un terremoto, de su visión de la mujer.
 <br><br>
 Después de esta novela, Carlos Cuauhtémoc Sánchez alternó la ficción narrativa con mensaje que había caracterizado su 
 producción con ensayos sobre crecimiento personal en los que trató temas diversos, como la productividad, la pareja, el amor y 
 la autorrealización: Te desafío a prosperar (2006), Te desafío a disfrutar el amor (2007), ¿Free Sex? La pregunta de hoy (2008), 
-Ser feliz es la meta (2011) y Tiempo de ganar (2013), aunque en algunos, como Luz en la tormenta (2009), la exposición se combina con historias ficticias.
+Ser feliz es la meta (2011) y Tiempo de ganar (2013),<p> aunque en algunos, como Luz en la tormenta (2009), la exposición se combina con historias ficticias.
 Entre sus últimas novelas cabe citar Los fantasmas del espejo (2008), sobre una chica anoréxica; Decisión crucial (2010); El virus (2010), 
 historia de un hombre que, tras contraer una extraña enfermedad, indaga en el sentido del sufrimiento; El feo (2010); Mientras respire (2014),
  sobre tres mujeres al borde del suicidio, y Si quieres casarte con mi hija, debemos hablar (2015), en que un padre se dirige al novio de su hija.<br>
