e1988d70cba8c2e1219f6077c113f1c79e263d45
GuilianavaD <guilianavaler@gmail.com>
2021-01-21 13:14:33 -0500

    Create leerdaniel.php

---
+++ leerdaniel.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA</title>
+    <style type="text/css">
+    body{
+        margin-left: 40px;
+        margin-right: 40px;
+        margin-top: 40px;
+        margin-bottom: 30px;
+    }
+    table{
+        background-color:#ddd;
+        float:left;
+        text-align: justify; 
+    }
+    #cabe {
+        color:#ff9;
+        background-color:#000;
+        padding:10px;
+      }
+    #pie {
+        background-color:#000;
+        padding:10px;
+      }
+    </style>
+    </head>
+    <body>
+        <div id="cabe">
+    <h1>&nbsp;&nbsp;&nbsp;BIOGRAFÍA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="autores.php"><input type='submit' value='regresar'></a></h1> </div>
+    <table><tr><th>
+     <center><h1>Daniel Schreiber</h1> 
+   <img style="border-radius: 20px;"src="imagen/daniel_ultima.jpg"aling="left" width="390px" height="180" alt="" ></center><p>
+    Daniel Schreiber nació en Mecklemburgo en 1977. Es escritor y periodista alemán. Trabaja como crítico de arte para diferentes 
+    periódicos y revistas internacionales como Die Zeit o Philosophie Magazin, además de trabajar en la emisora de radio Deutschlandradio Kultur.
+    <p>Se dio a conocer por su biografía de Susan Sontag, Intelectualidad y glamour (2007),
+    y por su personal ensayo sobre el alcoholismo, La última copa (2014), que se convirtió en un éxito de ventas en Alemania ese 
+    mismo año. Su último libro es Zuhause: Die Suche nach dem Ort, an dem wir leben wollen (2017).<p>Actualmente vive en Berlín.
+    internacionales . En el pasado fue editor de Monopol y dirigió la sección cultural de la revista Cicero.
+     </table></tr></th><br>
+<div id="pie">
+    hola
+    </div>
+    </body>
+</html>
