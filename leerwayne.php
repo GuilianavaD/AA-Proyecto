cad34f97b9b037b24c781a73c6e64ae819a31f5a
GuilianavaD <guilianavaler@gmail.com>
2021-01-21 13:14:42 -0500

    Update leerwayne.php

--- leerwayne.php
+++ leerwayne.php
@@ -33,19 +33,19 @@
     <table><tr><th>
     <img style="border-radius: 20px;"src="imagen/WayneDyerByPhilKonstantin.jpg"aling="left" width="290px" height="180" alt="" >
     <h1>Wayne Walter Dyer</h1> (Detroit, Míchigan; 10 de mayo de 1940 - Condado de Maui, Hawái; 29 de agosto de 2015)
-     fue un psicólogo y escritor de libros de autoayuda estadounidense.<br>
+     fue un psicólogo y escritor de libros de autoayuda estadounidense.<br><p>
      Nació en Detroit en 1940, en un barrio pobre. Creció como huérfano y fue cuidado por un tío. Sirvió en la Marina estadounidense 
-     antes de estudiar en la Wayne State University, donde obtuvo su doctorado en psicología.<br> Trabajaba como profesor en la St. John's University 
-     de Nueva York cuando escribió su primer libro «Tus zonas erróneas» (1976).<br> Tras el éxito —vendió 35 millones de ejemplares en diferentes idiomas, 
+     antes de estudiar en la Wayne State University, donde obtuvo su doctorado en psicología.<br><p> Trabajaba como profesor en la St. John's University 
+     de Nueva York cuando escribió su primer libro «Tus zonas erróneas» (1976).<br><p> Tras el éxito —vendió 35 millones de ejemplares en diferentes idiomas, 
      permaneciendo 64 semanas consecutivas como superventas en el New York Times— abandonó su puesto de profesor en St. John's para dedicarse a escribir y promocionar sus obras.
     Murió en Maui, Hawái, el 29 de agosto de 2015, a los 75 años, de leucemia linfática crónica, enfermedad que le había sido diagnosticada en 2009.
-    <h1>Fundamentos de sus obras y críticas</h1>
+    <p><h1>Fundamentos de sus obras y críticas</h1><p>
     Su inspiración fue Swami Muktananda, fundador del camino espiritual de Siddha Yoga, así como San Francisco de Asís y el filósofo chino Lao-Tse,
-     fundador del taoísmo.​ Su fundamento en la psicología transpersonal y en su fundador, Abraham Maslow, ha sido puesto en duda.<br> El psicólogo cognitivo L.
+     fundador del taoísmo.​ Su fundamento en la psicología transpersonal y en su fundador, Abraham Maslow, ha sido puesto en duda.<br><p> El psicólogo cognitivo L.
      Michael Hall argumenta que ya en sus primeras obras, Dyer malinterpretó su sentido e introdujo conceptos propios que la tergiversan o incluso falsean,
       aunque se le reconoce que el lenguaje asequible permitía, a personas sin formación previa, entender y practicar conceptos de la psicología cognitiva que
-     ya se habían demostrado útiles en la práctica psicoterapéutica.<br> Progresivamente, en sus obras posteriores a la primera trilogía se va haciendo más patente 
-     una influencia de la corriente llamada Nuevo Pensamiento.<br> Dyer fue miembro de Public Broadcasting Services, que difundió sus charlas. A partir de 2006 arreciaron 
+     ya se habían demostrado útiles en la práctica psicoterapéutica.<br><p> Progresivamente, en sus obras posteriores a la primera trilogía se va haciendo más patente 
+     una influencia de la corriente llamada Nuevo Pensamiento.<br> <p>Dyer fue miembro de Public Broadcasting Services, que difundió sus charlas. A partir de 2006 arreciaron 
      las quejas acerca de que estaba promoviendo una cosmovisión religiosa específica, lo que iba en contra de las normas editoriales de PBS.
      </table></tr></th><br>
 <div id="pie">
