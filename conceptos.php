c5ac0d2e4e35da5cd4ca2d844f160fcb34a27dfb
GuilianavaD <guilianavaler@gmail.com>
2021-01-22 13:01:15 -0500

    Update conceptos.php

--- conceptos.php
+++ conceptos.php
@@ -4,23 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" type="text/css" href="style.contacto.css">
     <title>AA</title>
+    <style type="text/css">
+     { 
+        margin:0;
+        padding:0;
+      }
+      body {
+        margin-left: 30px;
+        margin-right: 55px;
+        margin-bottom: 30px;
+        background:#CEF6F5;
+        font-family:verdana;
+      }
+      h1 {
+        color:#c0c;
+        font-size:24px;
+      }
+      p {
+        font-size:16px;
+      }
+      #cabecera {
+        color:#ff9;
+        background-color:#1036BC;
+        padding:20px;
+      }
+      #contenedor {
+        margin:0 auto;
+      }
+      #contenido {
+        background-color:#97CCE4;
+        float:left;
+        height:700px;
+        width:600px;
+        padding:10px;
+        text-align: justify; 
+      }
+      #menu {
+        margin:0 auto;
+        background-color:#6194AB;
+        float:left;
+        height:700px;
+        padding:10px;
+        width:530px;
+      }
+    </style> 
     </head>
     <body>
     <div id="header">
         <ul class="nav">
             <li><a href="Inicio.php">INICIO</a>
                 <ul>
-                    <li><a href="Informacion.php">INFORMACIÓN</a>
+                    <li><a href="misivisi.php">MISIÓN Y VISIÓN</a>
                 
                     </li>
-                    <li><a href="contacto.php">CONTACTOS</a>
+                    <li><a href="Informacion.php">INFORMACIÓN</a>
                     
                     </li>
-                    <li><a href="misivisi.php">MISIÓN Y VISIÓN</a>
+                    <li><a href="contacto.php">CONTACTOS</a>
                     </li>
-                    <li><a href="index.html">Cerrar Sesión</a>
+                    <li><a href="index.html">CERRAR SESIÓN</a>
                 </ul>
             </li>
             <li><a href="conceptos.php">SÍNTOMAS</a>
@@ -47,16 +90,19 @@
   <body>
     <div id="contenedor">
       <div id="cabecera">
-      <center><p>Síntomas que se tienen que saber del Alcoholismo </p></center>
+      <h1><center><p>SÍNTOMAS QUE SE TIENEN QUE SABER DEL ALCOHOLISMO</p></center></h1>
       </div>
       <div id="menu">
-          <img style="border:15px solid; color: blue;"src="imagen/alco.jpg"aling="left" width="120px" height="180" alt="" >
-          <img style="border:15px solid; color: blue;"src="imagen/alll.jfif"aling="left" width="120px" height="160" alt="" >
-          <img style="border:15px solid; color: blue;"src="imagen/bas.jpg"aling="left" width="120px" height="120" alt="" >
-          <img style="border:15px solid; color: blue;"src="imagen/atadura.jpg"aling="left" width="120px" height="90" alt="" >
-      </div>
+     <h1> BUSCA AYUDA PROFESIONAL <br><br></h1>
+          <img style="border-radius: 20px;border:10px solid; color:#E4E3C3;"src="imagen/2ima.jpg"aling="left" width="170px" height="180" alt="" >&nbsp;&nbsp;&nbsp;
+          <img style="border-radius: 20px;border:10px solid; color:#E4E3C3;"src="imagen/1ima.jfif"aling="left" width="170px" height="180" alt="" >&nbsp;&nbsp;&nbsp;
+          <img style="border-radius: 20px;border:10px solid; color:#E4E3C3;"src="imagen/4ima.jpg"aling="left" width="170px" height="180" alt="" >&nbsp;&nbsp;&nbsp;
+          <img style="border-radius: 20px;border:10px solid; color:#E4E3C3;"src="imagen/3ima.jpg"aling="left" width="170px" height="180" alt="" >&nbsp;&nbsp;&nbsp;
+          <img style="border-radius: 20px;border:10px solid; color:#E4E3C3;"src="imagen/5ima.jpg"aling="left" width="170px" height="180" alt="" >&nbsp;&nbsp;&nbsp;
+          <img style="border-radius: 20px;border:10px solid; color:#E4E3C3;"src="imagen/6ima.jpg"aling="left" width="170px" height="180" alt="" >&nbsp;&nbsp;&nbsp;
+     </div>
       <div id="contenido">
-      <h1>SÍNTOMAS</h1><br>
+      <center><h1>SÍNTOMAS</h1><br></center>
         <p>*Incapacidad de mantenerse sin consumir alcohol.<br><br>
         *Promesas incumplidas de controlarse o abstenerse.<br><br>
         *Descuido en la higiene personal y la alimentación.<br><br>
@@ -64,14 +110,14 @@
         *Ausencia, errores y retrasos recurrentes en el trabajo.<br><br>
         *Angustia o presión en el pecho por las mañanas.<br><br>
         *Mentir sobre la cantidad ingerida o esconder botellas.<br><br>
-        *Pérdida de tiempo y/o dinero en planear el consumo, consumiendo o por la resaca.<br><br>
-        *Beber solo o juntarse con personas con las cuales el único vínculo es la bebida.<br><br>
-        *Tener algún tipo de consecuencia por el consumo de alcohol y no ser capaz de controlarse.<br><br>
+        *Pérdida de tiempo y/o dinero en planear el consumo,<p>consumiendo o por la resaca.<br><br>
+        *Beber solo o juntarse con personas con las cuales el<p> único vínculo es la bebida.<br><br>
+        *Tener algún tipo de consecuencia por el consumo de <p>alcohol y no ser capaz de controlarse.<br><br>
         *Sentimientos de culpabilidad el día siguiente.<br><br>
         *Lagunas mentales.<br><br>
         *Ponerse a la defensiva al hablar sobre el problema.<br><br>
-        *Incapacidad para divertirse sin la presencia del alcohol.<br><br>
-        *Episodios de cambio de personalidad bajo los efectos del alcohol.<br><br></p>
+        *Incapacidad para divertirse sin la presencia del <p>alcohol.<br><br>
+        *Episodios de cambio de personalidad bajo los efectos <p>del alcohol.<br><br></p>
       </div>
     </div>
     
