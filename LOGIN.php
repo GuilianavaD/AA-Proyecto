f1b55c09a2a963b8daa438a262fef2e88fd24772
GuilianavaD <guilianavaler@gmail.com>
2021-01-24 19:20:36 -0500

    Update LOGIN.php

--- LOGIN.php
+++ LOGIN.php
@@ -6,6 +6,8 @@ echo <<<_END
 <html>
     <head>
         <title>AA</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
     <body style=background-image:url('imagen/fondoinvi.jpg width=100% height=100vh ')>
     <div class="box_form">
@@ -18,6 +20,9 @@ echo <<<_END
     </form>
     <div>
     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
     </body>
 </html>
 _END;
