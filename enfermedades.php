dfa2c338afd132f243c4bc8b53e3f32595d86172
GuilianavaD <guilianavaler@gmail.com>
2021-01-28 19:36:48 -0500

    Create enfermedades.php

---
+++ enfermedades.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+   
+    <title>AA</title>
+    <style type="text/css">
+     body{ background-color:#E8F4FA;
+        margin-left: 40px;
+        margin-right: 40px;
+        margin-top: 0px;
+        margin-bottom: 30px;
+    }
+    h2{
+        text-align: justify; 
+        color:#06205E;
+    }
+    #enfe{  
+
+        background-color:#ddd;
+        text-align: justify; 
+        height:auto;
+        width:auto;
+        padding:10px;
+    
+    }
+    #con{
+        background-color:#CCFBCC;
+        text-align: justify; 
+        padding:10px;
+    }
+     </style>
+    </head>
+    <body>
+    <div id="header">
+        <ul class="nav">
+            <li><a href="Inicio.php">INICIO</a>
+                <ul>
+                    <li><a href="misivisi.php">MISIÓN Y VISIÓN</a>
+                
+                    </li>
+                    <li><a href="Informacion.php">INFORMACIÓN</a>
+                    
+                    </li>
+                    <li><a href="contacto.php">CONTACTOS</a>
+                    </li>
+                    <li><a href="index.html">CERRAR SESIÓN</a>
+                </ul>
+            </li>
+            <li><a href="conceptos.php">SÍNTOMAS</a>
+                <ul>
+                <li><a href="alcoholismo.php">EL ALCOHOLISMO</a>
+                </li>
+                </ul>
+            </li>
+
+            <li><a href="consejos.php">CONSEJOS</a>
+                <ul> 
+                    <li><a href="recomendacion.php">RECOMENDACIONES</a>
+                </ul>
+            </li>
+            <li><a href="autores.php">AUTORES</a>
+                <ul> 
+                    <li><a href="libros.php">LIBROS</a>
+                </ul>
+            </li>
+    </div>
+    <br>
+    <br>
+    <br>
+    <center><h1>Enfermedades causadas por el alcohol</h1></center><br><br>
+    <div id="con"><br>
+      Enfermedad crónica caracterizada por la ingesta descontrolada de alcohol y preocupación por el consumo.
+El alcoholismo es la incapacidad de controlar el consumo de alcohol debido a una dependencia física y emocional.
+Los síntomas incluyen consumo recurrente de alcohol a pesar de los problemas legales y de salud relacionados.
+ Las personas alcohólicas pueden beber al comienzo del día, sentirse culpables por el consumo y tener el deseo
+  de reducir la cantidad de alcohol que ingieren.
+El tratamiento incluye el asesoramiento por parte de un profesional del cuidado de la salud. Una opción para
+ aquellos que necesitan ayuda adicional son los programas de desintoxicación en hospitales o instalaciones médicas.
+  Existen medicamentos que reducen el deseo de consumir alcohol.<br>
+  <br><a href="imagen/enfermedadesdelalcohol-130207051427-phpapp02-thumbnail-4.jpg"><center><img style="border-radius: 20px;"src="imagen/enfermedadesdelalcohol-130207051427-phpapp02-thumbnail-4.jpg"width="400px" height="auto" alt="" ></center></a><br>
+  <br><a href="imagen/consecuencias-del-alcoholismo-en-la-salud.jpg"><center><img style="border-radius: 20px;"src="imagen/consecuencias-del-alcoholismo-en-la-salud.jpg"width="400px" height="autopx" alt="" ></center><br></a>
+  
+</div>
+<div id="enfe">
+    <h3>Hay más de tres millones de muertes al año por consumo de alcohol en el mundo.
+     Muchas personas enferman a consecuencia de sus efectos nocivos. Muchas de las enfermedades que causa son mortales. 
+
+Las consecuencias más inmediatas para la salud impactan directamente en el corazón,el hígado y el sistema nervioso.
+ A corto plazo crea problemas de memoria y disfunciones sexuales.</h3><br>
+ <center><img style="border-radius: 20px;"src="imagen/alcohol222.jpg"width="400px" height="200" alt="" ></center><br>
+       
+<h2> 1. Trastorno por adicción al alcohol o alcoholismo</h2><p><br>
+Una de las enfermedades enfermedades psiquiátricas más graves que produce su consumo es el propio alcoholismo.
+ Es un trastorno que convierte a la persona en un esclavo de esta sustancia mientras además, destruye su organismo. 
+ <br> Se caracteriza por:<p>
+ <br>
+*Evidencia subjetiva de compulsión para la utilización del alcohol durante los intentos de abandono del consumo. 
+Su vida queda centrada en el deseo compulsivo de la obtención y consumo de alcohol. <p><br>
+*El deseo de abandonarlo.<p><br>
+*Tomar es un hábito estereotipado, un patrón de consumo y un estilo de vida.<p><br>
+*Evidencia de neuroadaptación, con fenómenos de tolerancia y dependencia.<p><br>
+*Se consume para aliviar o evitar los síntomas del síndrome de abstinencia alcohólica.<p><br>
+*Predominio de la conducta de búsqueda y obtención del alcohol por encima de otras prioridades importantes.<p><br>
+*Una rápida instauración de la dependencia si después de un periodo de abstinencia el paciente vuelve a consumir. <p><br>
+ 
+<br>
+<h2>2. Enfermedades del sistema nervioso</h2><p><br>
+Es un depresor del sistema nervioso central. Por tanto puede provocar, entre otras, enfermedades graves como:
+<p><br>
+*Depresión<p><br>
+*Insomnio<p><br>
+*Ataques de pánico<p><br>
+*Celotipia alcohólica (es común desarrollar unos celos enfermizos con el consumo de alcohol)<p><br>
+*Ansiedad<p><br>
+*Demencias neurodegenerativas como la enfermedad de Alzheimer o Parkinson. <p><br>
+*Deterioro cognitivo, pérdida de funciones cerebrales claves como la atención, la memoria, la dedicación, 
+la motivación, la toma de decisiones, etc.<p><br>
+*La sobredosis de alcohol puede producir daños cerebrales: <p><br>
+&nbsp;&nbsp;&nbsp;&nbsp;*Neuropatía alcohólica<p><br>
+&nbsp;&nbsp;&nbsp;&nbsp;*Demencia alcohólica. Síndrome de Korsakoff y Wernicke. <p><br>
+<h2>3. Enfermedades cardiovasculares</h2><p><br>
+Las personas que abusan del etanol tienen un mayor riesgo de, entre otros:<br>
+*Hipertensión arterial<br>
+*Accidentes cerebrovasculares <p><br>
+*Insuficiencia cardíaca, arritmia<p><br>
+*Miopatía alcohólica aguda y crónica <p><br>
+*Miocardiopatía alcohólica. El infarto. <br>
+<h2>4. Cáncer</h2><p><br>
+Entre otros, provoca:<p><br>
+
+*Cáncer de esófago<p><br>
+*Cáncer de estómago<p><br>
+*De colon y  recto<p><br>
+*Cáncer de hígado (debido a su implicación en la metabolización del alcohol, es el órgano más directamente 
+dañado y relacionado con un mayor número de muertes).<p><br>
+*De la cavidad oral <p><br>
+*Cáncer de pulmón  <p><br>
+*Cáncer de mama<p><br>
+ <br>
+
+<h2>5. Enfermedades del sistema digestivo</h2><p><br>
+Está directamente relacionado con el cáncer de todo el aparato digestivo. 
+
+Afecta a todo el aparato digestivo produciendo desde inflamaciones hasta hepatitis y pancreatitis. Puede desarrollar:
+&nbsp;&nbsp;&nbsp;&nbsp;<img style="border-radius: 20px;" src="imagen/unnamed (1).jpg" align="right" width="400px" height="200" alt="" ><br>
+*Pancreatitis crónica<p><br>
+*Hígado graso<p><br>
+*Hepatitis alcohólica<p><br>
+ 
+<br>
+<h2>6. Intoxicación alcohólica aguda</h2><p><br>
+Necesita atención médica inmediata. Afecta el sistema nervioso. Es una pérdida de control temporal tanto físico como mental
+ que puede llegar al coma etílico, con riesgo de muerte. <br>
+
+Los síntomas serían: inconsciencia, ausencia de reflejos, hipotensión, presión arterial baja e hipotermia. Se puede complicar
+ por posible traumatismo cráneo-encefálico a consecuencia de una caída, intoxicaciones mixtas, neumonía etc. Puede producir parálisis. <br>
+<br>
+<h2>7. Otros</h2><br>
+Como por ejemplo: <br>
+<p><br>
+1.-Traumatismos craneoencefálicos<p><br>
+2.-Disfunciones sexuales <p><br>
+3.-Desnutrición y anemia<p><br>
+4.-Accidentes de circulación y muerte de terceros <p><br>
+5.-Síndrome alcohólico fetal o SAF (consumir alcohol en el embarazo puede causar un conjunto de anormalidades en el feto)<p><br>
+ 
+
+<h2>8. Enfermedad social</h2><p><br>
+Podríamos considerar el abuso de alcohol como una enfermedad social, debido al grave impacto que causa en el entorno del paciente.
+ Está detrás de numerosas agresiones, violencia, peleas, destrozos materiales, absentismo laboral, accidentes domésticos, accidentes 
+ laborales, incendios, ahogos y suicidios.  <br>
+
+ <br>
+
+<h2>9. Enfermedad familiar</h2><br><p>
+Si la sociedad se ve afectada por el consumo de alcohol, este también tiene graves consecuencias en la familia del adicto.
+<br>
+<h2>1. La codependencia</h2><br>
+El alcoholismo provoca también una enfermedad familiar llamada codependencia. Se da en algunas de las personas cercanas al alcohólico. 
+El alcohol hace enfermar a toda una familia. En algún miembro de esta familia puede llegar a ser una verdadera patología. <br>
+Entre sus síntomas encontramos:<p><br>
+
+*Depresión<p><br>
+*Nerviosismo<p><br>
+*Ansiedad<p><br>
+*Automedicación<p><br>
+*Baja autoestima<p><br>
+*Excesiva dependencia<p><br>
+*Imposibilidad de poner límites<p><br>
+*Compulsividad<p><br>
+*Inestabilidad emocional<p><br>
+Otros<p><br>
+Esta enfermedad requiere de un tratamiento y de ayuda psicológica.<p><br>
+
+<h2>2. Rupturas de pareja</h2><p><br>
+La inestabilidad emocional sufrida por el alcohólico, los problemas de comunicación y el agotamiento de los recursos afectivos
+ destruyen familias. <p><br>
+
+<h2>3. Trastornos en los hijos</h2><p><br>
+ 
+<br>
+Si bebes habitualmente o bebes mucho y no sabes si has desarrollado el alcoholismo, acude a tu médico. En cualquier caso,
+ si no lo has desarrollado, lo más sano sería reducir el consumo para evitar la larga lista de consecuencias que acabamos de presentarte.
+</div>
+</body>
+</html>
